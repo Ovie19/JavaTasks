b0d7de50e68c86f1d1ce4a18764cf2b8cd02cf29
Ovie19 <adetolabenjamin18@gmail.com>
2026-04-20 08:20:51 +0100

    Kata update

--- TouchGrass/Kata.java
+++ TouchGrass/Kata.java
@@ -1,4 +1,36 @@
 public class Kata {
+    public static void main(String[] args) {
+        int maximum = maximum(45, 32);
+        System.out.println("The maximum of 45 and 32 is " + maximum);
+
+        boolean isEvenNumber = isEven(21);
+        System.out.println(isEvenNumber ? "21 is even": "21 is odd");
+
+        boolean isPrimeNumber = isPrimeNumber(21);
+        System.out.println(isPrimeNumber ? "21 is a prime number": "21 is not a prime number");
+
+        int difference = subtract(7, 3);
+        System.out.println("The difference between 7 and 3 is " + difference);
+
+        int quotient = subtract(5, 2);
+        System.out.println("The integer division of 5 by 2 is " + quotient);
+
+        int numberOfFactors = factorOf(69);
+        System.out.println("The number of factors of 69 is " + numberOfFactors);
+
+        boolean isPerfectSquare = isPerfectSquare(49);
+        System.out.println(isPerfectSquare ? "49 is a perfect number": "49 is not a perfect number");
+
+        boolean isPalindrome = isPalindrome(12321);
+        System.out.println(isPalindrome ? "12321 is a palindrome": "12321 is not a palindrome");
+
+        long factorial = factorialOf(20);
+        System.out.println("The factorial of 20 " + factorial);
+
+        long square = squareOf(20);
+        System.out.println("The square of 20 " + square);
+    }
+
     public static int maximum(int firstNumber, int secondNumber) {
         int maximum = firstNumber;
 
